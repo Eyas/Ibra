da9893d7f94d9ed740dfc0b16dced190ada17d4d
Eyas <eyas.sharaiha@gmail.com>
2015-07-24 22:57:22 -0400

    Initial commit: Ibra.Comparers and Ibra.Lazy libraries.
    Ibra.Lazy provides memoized Functions, similar to Lazy<T>. Ibra.Lazy.LazyFunc also detects cases of infinite recursion and provides an appropriate exception message, similar to C#'s Lazy<T>.
    
    Ibra.Comparers provides a number of comparers for lists: Equality and Ordering comparisons. Provides two ordering comparers with different semantics.

---
+++ IbraLib/src/Ibra.Comparers/Comparers.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+
+namespace Ibra.Comparers
+{
+    /// <summary>
+    /// Compares two lists according to some comparer.
+    /// </summary>
+    /// <typeparam name="T">Contained type in the Lists being compared.</typeparam>
+    public class OrderedEquator<T> : IEqualityComparer<IList<T>>
+    {
+        /// <summary>
+        /// Creates an OrderedEquator with the Default equality comparer for the elements.
+        /// </summary>
+        /// <remarks>Will use T.Equals() and T.GetHashCode()</remarks>
+        public OrderedEquator()
+        {
+            _cmp = EqualityComparer<T>.Default;
+        }
+        /// <summary>
+        /// Creates an OrderedEquator with a specified comparer
+        /// </summary>
+        /// <param name="comparer">
+        /// The backing comparer used to compare individual elements of the sequence
+        /// for equality.
+        /// </param>
+        public OrderedEquator(IEqualityComparer<T> comparer)
+        {
+            _cmp = comparer;
+        }
+        public bool Equals(IList<T> a, IList<T> b)
+        {
+            if (a == b) return true;
+            if (a == null) return false;
+            if (b == null) return false;
+
+            int ca = a.Count, cb = b.Count;
+            if (ca != cb) return false;
+
+            for (int i = 0; i < ca; i++)
+            {
+                if (!_cmp.Equals(a[i], b[i])) return false;
+            }
+            return true;
+        }
+        public int GetHashCode(IList<T> x)
+        {
+            if (x == null) return -1;
+            int h = 0;
+            foreach (T item in x)
+            {
+                h = h ^ _cmp.GetHashCode(item);
+            }
+            return h;
+        }
+        private readonly IEqualityComparer<T> _cmp;
+    }
+    /// <summary>
+    /// Compares two lists according to some comparer, in a left-padded lexicographic order.
+    /// </summary>
+    /// <remarks>
+    /// Left-padded lexicographic ordering is the ordering used to sort positive decimal
+    /// integers. Longer sequences are always greater than shorter sequences. This is unlike
+    /// typical (dictionary) lexicographic ordering, where lists of different lengths are
+    /// compared based on the values of individual items, assuming each is right-padded with
+    /// elements of smaller orderings.
+    /// </remarks>
+    /// <example>
+    /// When comparing lists of integers, we will get:
+    /// [1, 2, 3] EQ [ 1, 2, 3]
+    /// [0, 2, 3] LT [ 1, 2, 3]
+    /// [2, 2, 3] GT [ 1, 2, 3]
+    /// [   2, 3] LT [ 1, 2, 3]
+    /// [   2, 3] LT [ 0, 2, 3]
+    /// [   2, 3] LT [-1, 2, 3]
+    /// [1, 2, 3] LT [ 1, 2,-3]
+    ///    null   LT [ 1, 2, 3]
+    ///    null   EQ    null
+    /// </example>
+    /// <typeparam name="T">Contained type in the Lists being compared.</typeparam>
+    public class OrderedComparer<T> : IComparer<IList<T>>
+    {
+        public OrderedComparer()
+        {
+            _cmp = Comparer<T>.Default;
+        }
+        public OrderedComparer(IComparer<T> comparer)
+        {
+            _cmp = comparer;
+        }
+        public int Compare(IList<T> a, IList<T> b)
+        {
+            if (a == b) return 0;
+            if (a == null) return -1;
+            if (b == null) return  1;
+
+            int ca = a.Count, cb = b.Count;
+            if (ca > cb) return  1;
+            if (cb > ca) return -1;
+
+            for (int i = 0; i < ca; ++i)
+            {
+                int r = _cmp.Compare(a[i], b[i]);
+                if (r != 0) return r;
+            }
+            return 0;
+        }
+        private readonly IComparer<T> _cmp;
+    }
+    /// <summary>
+    /// Compares two lists according to some comparer, in a (right-padded) lexicographic order.
+    /// </summary>
+    /// <remarks>
+    /// Right-padded lexicographic order is also known as 'dictionary ordering' and referred to
+    /// simply as 'lexicographic ordering' as well. Sequences of different lengths are compared
+    /// element by element. If one sequence is a prefix of the other, the longer sequence comes
+    /// after.
+    /// </remarks>
+    /// <example>
+    /// When comparing lists of letters, we will get:
+    /// [B, B, C] EQ [B, B, C]
+    /// [A, B, C] LT [B, B, C]
+    /// [C, B, C] GT [B, B, C]
+    /// [B, B   ] LT [B, B, C]
+    /// [B, C   ] GT [B, B, C]
+    /// [B, A   ] LT [B, B, C]
+    ///    null   LT [B, B, C]
+    ///    null   EQ   null
+    /// </example>
+    /// <typeparam name="T">Contained type in the Lists being compared.</typeparam>
+    public class LexicographicalComparer<T> : IComparer<IList<T>>
+    {
+        public LexicographicalComparer()
+        {
+            _cmp = Comparer<T>.Default;
+        }
+        public LexicographicalComparer(IComparer<T> comparer)
+        {
+            _cmp = comparer;
+        }
+        public int Compare(IList<T> a, IList<T> b)
+        {
+            if (a == b) return 0;
+            if (a == null) return -1;
+            if (b == null) return 1;
+
+            int ca = a.Count, cb = b.Count;
+            int min = System.Math.Min(ca, cb);
+
+            for (int i = 0; i < min; ++i)
+            {
+                int r = _cmp.Compare(a[i], b[i]);
+                if (r != 0) return r;
+            }
+
+            return
+                (ca == cb) ? 0 :
+                (ca >  cb) ? 1 : -1;
+        }
+        private readonly IComparer<T> _cmp;
+    }
+}
---
+++ IbraLib/src/Ibra.Lazy/LazyFunc.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ibra.Lazy
+{
+    /// <summary>
+    /// Provides support for memoized functions, computed once for each unique input.
+    /// </summary>
+    /// <typeparam name="T">Input type</typeparam>
+    /// <typeparam name="R">Return type</typeparam>
+    public interface ILazyFunc<in T, out R>
+    {
+        R this[T input] { get; }
+        R Get(T input);
+    }
+
+    /// <seealso cref="ILazyFunc{T, R}"/>
+    public interface ILazyFunc<in T1, in T2, out R>
+    {
+        R Get(T1 arg1, T2 arg2);
+    }
+
+    /// <seealso cref="ILazyFunc{T, R}"/>
+    public interface ILazyFunc<in T1, in T2, in T3, out R>
+    {
+        R Get(T1 arg1, T2 arg2, T3 arg3);
+    }
+
+    /// <seealso cref="ILazyFunc{T, R}"/>
+    public interface ILazyFunc<in T1, in T2, in T3, in T4, out R>
+    {
+        R Get(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
+    }
+
+    /// <seealso cref="ILazyFunc{T, R}"/>
+    public interface ILazyFunc<in T1, in T2, in T3, in T4, in T5, out R>
+    {
+        R Get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
+    }
+
+    /// <seealso cref="ILazyFunc{T, R}"/>
+    public interface ILazyFunc<in T1, in T2, in T3, in T4, in T5, in T6, out R>
+    {
+        R Get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6);
+    }
+
+    /// <seealso cref="ILazyFunc{T, R}"/>
+    public interface ILazyFunc<in T1, in T2, in T3, in T4, in T5, in T6, in T7, out R>
+    {
+        R Get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7);
+    }
+
+    public sealed class LazyFunc<T, R> : ILazyFunc<T, R>
+    {
+        #region Internals
+        private sealed class ResultInfo // needs to be a mutable reference type
+        {
+            public bool HasValue = false;
+            public R Value = default(R);
+        }
+        /// <summary>
+        /// Uncached version of our function
+        /// </summary>
+        private Func<T, R> _func;
+        /// <summary>
+        /// There are three states in this dictionary:
+        /// (1) t is not in the dictionary: uncached
+        /// (2) t is in the dictionary, with value Nothing-- faulted but not cached; don't infinitely recurse
+        /// (3) t is in the dictionary, with a defined value-- cached and can be safely returned.
+        /// </summary>
+        private Dictionary<T, ResultInfo> _lazy;
+        #endregion
+
+        public LazyFunc(Func<T, R> func, IEqualityComparer<T> comparer)
+        {
+            _func = func;
+            _lazy = new Dictionary<T, ResultInfo>(comparer);
+        }
+        public LazyFunc(Func<T, R> func)
+        {
+            _func = func;
+            _lazy = new Dictionary<T, ResultInfo>();
+        }
+        public R this[T input]
+        {
+            get
+            {
+                ResultInfo result;
+                if (_lazy.TryGetValue(input, out result))
+                {
+                    if (result.HasValue) return result.Value;
+                    else throw new InvalidOperationException("Attemptign to recursively get value from LazyFunc.");
+                } // all code paths return/throw
+
+                result = new ResultInfo(); // HasValue = false on construction
+                _lazy.Add(input, result); // add before calling to detect recursive calls
+                R output = _func(input);
+                result.Value = output;
+                result.HasValue = true;
+                return output;
+            }
+        }
+        public R Get(T input) => this[input];
+    }
+    public sealed class LazyFunc<T1, T2, R> : ILazyFunc<T1, T2, R>
+    {
+        private LazyFunc<Tuple<T1, T2>, R> _lazy;
+
+        public LazyFunc(Func<T1, T2, R> func, IEqualityComparer<Tuple<T1, T2>> comparer)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2>, R>((tuple) => func(tuple.Item1, tuple.Item2), comparer);
+        }
+        public LazyFunc(Func<T1, T2, R> func)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2>, R>((tuple) => func(tuple.Item1, tuple.Item2));
+        }
+        public R Get(T1 arg1, T2 arg2)
+        {
+            var input = Tuple.Create(arg1, arg2);
+            return _lazy[input];
+        }
+    }
+    public sealed class LazyFunc<T1, T2, T3, R> : ILazyFunc<T1, T2, T3, R>
+    {
+        private LazyFunc<Tuple<T1, T2, T3>, R> _lazy;
+
+        public LazyFunc(Func<T1, T2, T3, R> func, IEqualityComparer<Tuple<T1, T2, T3>> comparer)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3), comparer);
+        }
+        public LazyFunc(Func<T1, T2, T3, R> func)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3));
+        }
+        public R Get(T1 arg1, T2 arg2, T3 arg3)
+        {
+            var input = Tuple.Create(arg1, arg2, arg3);
+            return _lazy[input];
+        }
+    }
+    public sealed class LazyFunc<T1, T2, T3, T4, R> : ILazyFunc<T1, T2, T3, T4, R>
+    {
+        private LazyFunc<Tuple<T1, T2, T3, T4>, R> _lazy;
+
+        public LazyFunc(Func<T1, T2, T3, T4, R> func, IEqualityComparer<Tuple<T1, T2, T3, T4>> comparer)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3, T4>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4), comparer);
+        }
+        public LazyFunc(Func<T1, T2, T3, T4, R> func)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3, T4>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4));
+        }
+        public R Get(T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+        {
+            var input = Tuple.Create(arg1, arg2, arg3, arg4);
+            return _lazy[input];
+        }
+    }
+    public sealed class LazyFunc<T1, T2, T3, T4, T5, R> : ILazyFunc<T1, T2, T3, T4, T5, R>
+    {
+        private LazyFunc<Tuple<T1, T2, T3, T4, T5>, R> _lazy;
+
+        public LazyFunc(Func<T1, T2, T3, T4, T5, R> func, IEqualityComparer<Tuple<T1, T2, T3, T4, T5>> comparer)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3, T4, T5>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5), comparer);
+        }
+        public LazyFunc(Func<T1, T2, T3, T4, T5, R> func)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3, T4, T5>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5));
+        }
+        public R Get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5)
+        {
+            var input = Tuple.Create(arg1, arg2, arg3, arg4, arg5);
+            return _lazy[input];
+        }
+    }
+    public sealed class LazyFunc<T1, T2, T3, T4, T5, T6, R> : ILazyFunc<T1, T2, T3, T4, T5, T6, R>
+    {
+        private LazyFunc<Tuple<T1, T2, T3, T4, T5, T6>, R> _lazy;
+
+        public LazyFunc(Func<T1, T2, T3, T4, T5, T6, R> func, IEqualityComparer<Tuple<T1, T2, T3, T4, T5, T6>> comparer)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3, T4, T5, T6>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6), comparer);
+        }
+        public LazyFunc(Func<T1, T2, T3, T4, T5, T6, R> func)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3, T4, T5, T6>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6));
+        }
+        public R Get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6)
+        {
+            var input = Tuple.Create(arg1, arg2, arg3, arg4, arg5, arg6);
+            return _lazy[input];
+        }
+    }
+    public sealed class LazyFunc<T1, T2, T3, T4, T5, T6, T7, R> : ILazyFunc<T1, T2, T3, T4, T5, T6, T7, R>
+    {
+        private LazyFunc<Tuple<T1, T2, T3, T4, T5, T6, T7>, R> _lazy;
+
+        public LazyFunc(Func<T1, T2, T3, T4, T5, T6, T7, R> func, IEqualityComparer<Tuple<T1, T2, T3, T4, T5, T6, T7>> comparer)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3, T4, T5, T6, T7>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7), comparer);
+        }
+        public LazyFunc(Func<T1, T2, T3, T4, T5, T6, T7, R> func)
+        {
+            _lazy = new LazyFunc<Tuple<T1, T2, T3, T4, T5, T6, T7>, R>((tuple) => func(tuple.Item1, tuple.Item2, tuple.Item3, tuple.Item4, tuple.Item5, tuple.Item6, tuple.Item7));
+        }
+        public R Get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7)
+        {
+            var input = Tuple.Create(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+            return _lazy[input];
+        }
+    }
+}
