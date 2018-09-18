71f8ad27ae74c4ab41bb8c5838c38b14ba5b4566
Eyas Sharaiha <eyas.sharaiha@gmail.com>
2018-09-17 22:12:26 -0400

    Documentation for each Enumerable Extension.

--- IbraLib/src/Ibra.Enumerables/Extensions.cs
+++ IbraLib/src/Ibra.Enumerables/Extensions.cs
@@ -6,9 +6,15 @@ namespace Ibra.Enumerables
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Flatten out a nested sequence of sequences.
+        /// </summary>
         public static IEnumerable<T> Flatten<T>(this IEnumerable<IEnumerable<T>> sequence) =>
             sequence.SelectMany(sublist => sublist);
 
+        /// <summary>
+        /// Return a sequence with only non-nullable values present.
+        /// </summary>
         public static IEnumerable<T> Flatten<T>(this IEnumerable<T?> sequence) where T : struct
         {
             foreach (var nullable in sequence)
@@ -17,9 +23,17 @@ namespace Ibra.Enumerables
             }
         }
 
+        /// <summary>
+        /// Project each sequence elemnt into a non-null result based on <param name="func"/>.
+        /// </summary>
         public static IEnumerable<T> SelectWhere<U, T>(this IEnumerable<U> sequence, Func<U, T?> func) where T : struct
             => sequence.Select(func).Flatten();
 
+        /// <summary>
+        /// Project a sequence into the corresponding value in the <paramref name="dictionary"/>
+        /// provided, skipping all keys in the input <paramref name="sequence"/> that don't exist
+        /// in the dictionary.
+        /// </summary>
         public static IEnumerable<V> TrySelectFromDictionary<K, V>(this IEnumerable<K> sequence, IReadOnlyDictionary<K, V> dictionary)
         {
             foreach (K key in sequence)
@@ -28,11 +42,25 @@ namespace Ibra.Enumerables
             }
         }
 
+        /// <summary>
+        /// Treat a <see cref="Nullable{T}"/> <paramref name="maybe"/> a sequence with
+        /// no elements (if null), or a single element (if not null).
+        /// </summary>
         public static IEnumerable<T> ToEnumerable<T>(this T? maybe) where T : struct
             => maybe.HasValue ? One(maybe.Value) : Enumerable.Empty<T>();
 
+        /// <summary>
+        /// Creates an Enumerable with a single element, <paramref name="item"/>.
+        /// </summary>
         public static IEnumerable<T> One<T>(T item) => new SingleEnumerable<T>(item);
 
+        /// <summary>
+        /// Project each element of <paramref name="sequence"/> into an element of
+        /// type <typeparamref name="TTo"/>, if the input element is of that type.
+        /// Skips every element where the cast to <typeparamref name="TTo"/> fails.
+        /// </summary>
+        /// <typeparam name="TFrom">Input runtime type of each element.</typeparam>
+        /// <typeparam name="TTo">Output type of each remaining element.</typeparam>
         public static IEnumerable<TTo> WhereCast<TFrom, TTo>(this IEnumerable<TFrom> sequence) where TTo : class, TFrom
         {
             IEnumerator<TFrom> e = sequence.GetEnumerator();
@@ -42,10 +70,21 @@ namespace Ibra.Enumerables
             }
         }
 
-        public static IEnumerable<ValueTuple<T1, T2>> Combine<T1, T2>(this IEnumerable<T1> first, IEnumerable<T2> second)
+        /// <summary>
+        /// Combines two input <see cref="IEnumerable{T}"/>s into a <see cref="ValueTuple{T1, T2}"/>
+        /// of both elements.
+        /// </summary>
+        /// <seealso cref="System.Linq.Enumerable.Zip{TFirst, TSecond, TResult}(IEnumerable{TFirst}, IEnumerable{TSecond}, Func{TFirst, TSecond, TResult})"/>
+        public static IEnumerable<(T1, T2)> Combine<T1, T2>(this IEnumerable<T1> first, IEnumerable<T2> second)
             => first.Zip(second, ValueTuple.Create);
 
-        public static IEnumerable<ValueTuple<int, T>> ZipWithIndex<T>(this IEnumerable<T> sequence)
+        /// <summary>
+        /// Projects each value into a <see cref="ValueTuple{T1, T2}"/> with its index.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sequence"></param>
+        /// <seealso cref="System.Linq.Enumerable.Zip{TFirst, TSecond, TResult}(IEnumerable{TFirst}, IEnumerable{TSecond}, Func{TFirst, TSecond, TResult})"/>
+        public static IEnumerable<(int, T)> ZipWithIndex<T>(this IEnumerable<T> sequence)
             => sequence.Select((elt, idx) => ValueTuple.Create(idx, elt));
     }
 }
